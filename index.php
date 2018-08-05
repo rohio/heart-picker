71771895fcdd97e7834e270d830f1e3b48b722e9
hiroshi <dreamofank@yahoo.co.jp>
2018-08-06 08:31:38 +0900

    入力値チェックを追加

--- index.php
+++ index.php
@@ -17,22 +17,23 @@
                 <form class="form" action="heart.php" method="post">
                     <label for="twitter_id" accesskey="n"><font size="5">TwitterID</font><font size="3">＊必須</font><br>
                     <font size="2" color="aqua">@ は付けないでください </font></label>
-                    <input type="text" name="twitter_id" placeholder="例: TwitterJP" id="twitter_id">
+                    <input type="text" name="twitter_id" placeholder="例: TwitterJP" id="twitter_id" required>
 
                     <label for="display_num" accesskey="n"><font size="4">表示件数</font><font size="3">＊必須</font><br>
                     <font size="2" color="lightcyan">1~200の間で指定してください</font></label>
-                    <input type="text" name="display_num" placeholder="例: 10" id="display_num"><br>
+                    <input type="number" name="display_num" placeholder="例: 10" id="display_num" min="1" max="200" required><br>
 
                     <label for="begin_date" accesskey="n"><font size="4">日付指定(開始日)　</font><font size="3">任意</font><br>
-                    <font size="2" color="lightcyan">yyyy/mm/ddの形式で指定してください</font></label>
-                    <input type="date" name="begin_date" placeholder="例: 2015/01/01" id="begin_date">
+                    <font size="2" color="lightcyan">Year-Month-Dayの形式で指定してください</font></label>
+                    <input type="text" name="begin_date" placeholder="例: 2015-1-1" id="begin_date" pattern="201[0-9][/-]([1-9]|0[1-9]|1[12])[/-]([1-9]|[0-2][1-9]|3[01])">
 
                     <label for="end_date" accesskey="n"><font size="4">日付指定(終了日)　</font><font size="3">任意</font><br>
-                    <font size="2" color="lightcyan">yyyy/mm/ddの形式で指定してください</font></label>
-                    <input type="text" name="end_date" placeholder="例: 2016/01/31" id="end_date">
+                    <font size="2" color="lightcyan">Year-Month-Dayの形式で指定してください</font></label>
+                    <input type="date" name="end_date" placeholder="例: 2016-1-31" id="end_date">
 
                     <br>
                     <button type="submit">Heart Pick!</button>
+
                 </form>                
             </div>
 
