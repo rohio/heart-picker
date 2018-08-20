e479f1c7e42b2396edcb13d2011d81de6346288e
hiroshi <dreamofank@yahoo.co.jp>
2018-08-20 22:29:13 +0900

    auth.phpのデバッグ中

--- auth.php
+++ auth.php
@@ -4,7 +4,6 @@
         <meta charset="UTF-8">
         <title>はーとぴっかー</title>
         <link rel="stylesheet/less" type="text/css" href="style.less">
-		<link rel="icon" href="favicon.ico">
         <meta name="viewport" content="width=device-width, user-scale=yes, initial-scale=1.0, maximum-scale=5.0" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
@@ -24,67 +23,36 @@
         <div class="wrapper">
             <div class="container">
 
-                <h1>Heart♡Picker</h1>
-                
-                HeartPickerは自分や友達のいいねをランダムに表示するサービスです。<br>
-				TwitterIDと日時範囲(任意)を指定して、｢Heart Pick!｣ボタンを押してね！
+                <h1>はーとぴっかー</h1> 
+                <h1>Heart♡Pickker</h1>
                 
                 <form class="form" action="heart.php" method="post">
-                    <label for="twitter_id" accesskey="n" class="item_EN">TwitterID　<span class="must">必須</span><br></label>
-                    <div class="input-group">
-                        <span class="input-group__addon">@</span>
-                        <input type="text" name="twitter_id" id="twitter_id" class="input-group__control" placeholder="例: TwitterJP">
-                    </div>
-                    <br>
+                    <label for="twitter_id" accesskey="n" class="item_EN">TwitterID　<span class="must">必須</span><br>
+                    <div class="explain">@ は付けないでください </div></label>
+                    <input type="text" name="twitter_id" placeholder="例: TwitterJP" id="twitter_id" required>
+
+                    <label for="display_num" accesskey="n" class="item_JP">表示件数　<span class="must">必須</span><br>
+                    <div class="explain">1~20 の間で指定してください</div></label>
+                    <input type="number" name="display_num" placeholder="例: 10" id="display_num" min="1" max="20" required><br>
 
                     <label for="begin_date" accesskey="n" class="item_JP">日付範囲(開始日)　<span class="free">任意</span><br>
-                    <div class="explain">Year-Month-Day の形式で指定してください<br>※｢2010-11-4｣より前は指定できません</div></label>
-                    <input type="text" name="begin_date" placeholder="例: 2015-1-1" id="begin_date" class="user_input" maxlength="10" onInput="checkForm(this)" pattern="(201[1-9][/-]([1-9]|0[1-9]|1[12])[/-]([1-9]|0[1-9]|[1-2][0-9]|3[01])|2010[/-]1(1[/-]([5-9]|0[5-9]|[1-2][0-9]|3[01])|2[/-]([1-9]|0[1-9]|[1-2][0-9]|3[01])))">
+                    <div class="explain">Year-Month-Day の形式で指定してください</div></label>
+                    <input type="text" name="begin_date" placeholder="例: 2015-1-1" id="begin_date" maxlength="10" onInput="checkForm(this)" pattern="201[0-9][/-]([1-9]|0[1-9]|1[12])[/-]([1-9]|0[1-9]|[1-2][0-9]|3[01])">
 
                     <label for="end_date" accesskey="n" class="item_JP">日付範囲(終了日)　<span class="free">任意</span><br>
-                    <div class="explain">Year-Month-Day の形式で指定してください<br>※｢2010-11-4｣より前は指定できません</div></label>
-                    <input type="text" name="end_date" placeholder="例: 2016-1-31" id="end_date" class="user_input" maxlength="10" onInput="checkForm(this)" pattern="(201[1-9][/-]([1-9]|0[1-9]|1[12])[/-]([1-9]|0[1-9]|[1-2][0-9]|3[01])|2010[/-]1(1[/-]([5-9]|0[5-9]|[1-2][0-9]|3[01])|2[/-]([1-9]|0[1-9]|[1-2][0-9]|3[01])))">
+                    <div class="explain">Year-Month-Day の形式で指定してください</div></label>
+                    <input type="text" name="end_date" placeholder="例: 2016-1-31" id="end_date" maxlength="10" onInput="checkForm(this)" pattern="201[0-9][/-]([1-9]|0[1-9]|1[12])[/-]([1-9]|0[1-9]|[1-2][0-9]|3[01])">
 
                     <br>
                     <button type="submit">Heart Pick!</button>
                 </form>
+
                 <form class="form" action="auth.php" method="get">
-                <details>
-                    <summary>詳しい使い方,仕様（クリックで展開）</summary>
-                    <div class="use">
-                        <ul>
-                        <li>TwitterIDは自分、他人のどちらも指定できます。</li>
-                        <li>日付範囲が未指定の場合、全件からランダムに選ばれます。</li>
-                        <li>TwitterAPIの仕様により、3200件より多くのいいねをしているユーザは、最近3200件のいいねの中からランダムに表示されます。</li>
-                        <li>非公開設定にしているアカウント（鍵アカウント）のいいねは表示できません。</li>
-                        <li>TwitterAPIの使用回数制限のため、サービスを実行する回数が多いと制限がかかり、サービスを利用できなくなります。
-                        より多く使用したい人は、文末のTwitterのアイコンを押して、Twitterでログインしアプリケーション認証を行ってください。
-                        <button class="login_twitter" type="submit"><img src="images/TwitterLogo.png" alt="Twitterでログイン" width="19" height="16"/></button></li>
-                        TwitterAPIの使用制限とTwitterのログインによるアプリケーション認証に関しての詳細を知りたい方は、本ページの末尾にて説明しますので、そちらを参照ください。</li>
-                        <li>日付範囲が未指定であったり、日付範囲が広いと、最近のものが選ばれる確率が少しだけ高くなります。</li><br>
-                        <details>
-                            <summary>使用制限,認証に関して（クリックで展開）</summary>
-                            <div class="limit">
-                                <ol>
-                                <li>使用回数制限に関して<br>
-                                TwitterAPIは、認証を行わない場合はアプリケーション単位、認証を行った場合はユーザ単位に対して使用回数が制限されます。
-                                アプリケーション単位の場合は、アプリケーションを複数のユーザが使用している場合、複数ユーザの合計の使用回数を基準としてTwitterAPIの使用が制限されます。
-                                ユーザ単位の場合は、アプリケーションを複数のユーザが使用している場合でも、1ユーザの使用回数を基準として、TwitterAPIの使用が制限されます。
-                                そのため、ユーザ認証を行えば、使用制限が緩和されます。ユーザ認証を行った場合の使用回数の目安ですが、最低でも15分間に15回のいいね表示を行うことができます。</li>
-                                <li>ユーザ認証(read only)に関して<br>
-                                HeartPickerはユーザ認証を行っても、権限を悪用しユーザの意図に反するようなこと（ツイートする、フォローを行う等）は行いません。
-                                しかし、1.で述べたようにHeartPickerはAPIの使用回数緩和のため、認証が必要となります。
-                                そこで、最低限の権限の認証で十分なため、read権限のみの認証を行います。
-                                以下は補足ですが、勝手にツイートがされてしまう、いわゆるスパムと呼ばれるものはread権限だけでなく、write権限を必要とします。
-                                そのため、HeartPickerはスパムと呼ばれるようなアプリの動作は権限の面で不可能となっています。</li>
-								</ol>
-                            </div>
-                        </details>
-                        </ul>
-                    </div>
-                </details>
+                    <button type="submit">認証</button>
                 </form>
             </div>
+
+
             <ul class="bg-bubbles-index">
                 <li></li>
                 <li></li>
@@ -98,18 +66,26 @@
                 <li></li>
             </ul>
         </div>
+        
+    </body>
+
 <?php
 $api_key = '5L41MwG316NQvDhd3ru1UDiIa'; 
 $api_secret = 'Y8daT5rjGsfQL49nHIzJKkL07Gq3BB2IAlR6NIl7owWSn00Lkz';
 $callback_url = "https://inputform.herokuapp.com/auth.php" ;	// Callback URL (このプログラムのURLアドレス)
+// $callback_url = "http://192.168.33.10:8000/" ;	// Callback URL (このプログラムのURLアドレス)
+
+/*** [手順4] ユーザーが戻ってくる ***/
 
 // 認証画面から戻ってきた時 (認証OK)
 if ( isset( $_GET['oauth_token'] ) || isset($_GET["oauth_verifier"]) ) {
-	/*** アクセストークンを取得する ***/
-	//[リクエストトークン・シークレット]をセッションから取得
+	/*** [手順5] [手順5] アクセストークンを取得する ***/
+
+	//[リクエストトークン・シークレット]をセッションから呼び出す
+	session_start() ;
 	$request_token_secret = $_SESSION["oauth_token_secret"] ;
 
-	// アクセストークンを取得するAPI
+	// リクエストURL
 	$request_url = "https://api.twitter.com/oauth/access_token" ;
 
 	// リクエストメソッド
@@ -133,24 +109,34 @@ if ( isset( $_GET['oauth_token'] ) || isset($_GET["oauth_verifier"]) ) {
 	foreach( $params as $key => $value ) {
 		$params[ $key ] = rawurlencode( $value ) ;
 	}
+
 	// 連想配列をアルファベット順に並び替え
 	ksort($params) ;
+
 	// パラメータの連想配列を[キー=値&キー=値...]の文字列に変換
 	$request_params = http_build_query( $params , "" , "&" ) ;
+
 	// 変換した文字列をURLエンコードする
 	$request_params = rawurlencode($request_params) ;
+
 	// リクエストメソッドをURLエンコードする
 	$encoded_request_method = rawurlencode( $request_method ) ;
+
 	// リクエストURLをURLエンコードする
 	$encoded_request_url = rawurlencode( $request_url ) ;
+
 	// リクエストメソッド、リクエストURL、パラメータを[&]で繋ぐ
 	$signature_data = $encoded_request_method . "&" . $encoded_request_url . "&" . $request_params ;
+
 	// キー[$signature_key]とデータ[$signature_data]を利用して、HMAC-SHA1方式のハッシュ値に変換する
 	$hash = hash_hmac( "sha1" , $signature_data , $signature_key , TRUE ) ;
+
 	// base64エンコードして、署名[$signature]が完成する
 	$signature = base64_encode( $hash ) ;
+
 	// パラメータの連想配列、[$params]に、作成した署名を加える
 	$params["oauth_signature"] = $signature ;
+
 	// パラメータの連想配列を[キー=値,キー=値,...]の文字列に変換する
 	$header_params = http_build_query( $params, "", "," ) ;
 
@@ -178,37 +164,54 @@ if ( isset( $_GET['oauth_token'] ) || isset($_GET["oauth_verifier"]) ) {
 	curl_close( $curl ) ;
 
 	// 取得したデータ
-	$response = substr( $res1, $res2["header_size"] ) ;
+	$response = substr( $res1, $res2["header_size"] ) ;	// 取得したデータ(JSONなど)
+	$header = substr( $res1, 0, $res2["header_size"] ) ;	// レスポンスヘッダー (検証に利用したい場合にどうぞ)
+
+	// [cURL]ではなく、[file_get_contents()]を使うには下記の通りです…
+	// $response = file_get_contents( $request_url , false , stream_context_create( $context ) ) ;
 
 	// $responseの内容(文字列)を$query(配列)に直す
 	// aaa=AAA&bbb=BBB → [ "aaa"=>"AAA", "bbb"=>"BBB" ]
 	$query = [] ;
 	parse_str( $response, $query ) ;
 
-	// 取得したアクセストークンをセッションの変数に格納
+	// アクセストークン
 	$_SESSION["oauth_token"] = $query["oauth_token"];
 
-	// 取得したアクセストークン・シークレットをセッションの変数に格納
+	// アクセストークン・シークレット
     $_SESSION["oauth_token_secret"] = $query["oauth_token_secret"];
     
+    // DEBUG
+    echo $_SESSION["oauth_token"];
+    echo $_SESSION["oauth_token_secret"];
+
 	// ユーザーID
 	// $query["user_id"]
 
 	// スクリーンネーム
 	// $query["screen_name"]
 
-// 認証画面から戻ってきた時 (認証NG) 特に何も処理をしない
+	// 配列の内容を出力する (本番では不要)
+	echo '<p>下記の認証情報を取得しました。(<a href="' . explode( "?", $_SERVER["REQUEST_URI"] )[0] . '">もう1回やってみる</a>)</p>' ;
+
+	foreach ( $query as $key => $value ) {
+		echo "<b>" . $key . "</b>: " . $value . "<BR>" ;
+	}
+
+// 認証画面から戻ってきた時 (認証NG)
 } elseif ( isset( $_GET["denied"] ) ) {
-	// TODO return でOK?
+	// エラーメッセージを出力して終了
+	echo "連携を拒否しました。" ;
 	exit ;
+
 // 初回のアクセス
 } else {
-	/*** リクエストトークンの取得 ***/
+	/*** [手順1] リクエストトークンの取得 ***/
 
-	// [アクセストークンシークレット] (まだ存在しないので空文字)
+	// [アクセストークンシークレット] (まだ存在しないので「なし」)
 	$access_token_secret = "" ;
 
-	// リクエストトークンを取得するAPI
+	// エンドポイントURL
 	$request_url = "https://api.twitter.com/oauth/request_token" ;
 
 	// リクエストメソッド
@@ -240,22 +243,31 @@ if ( isset( $_GET['oauth_token'] ) || isset($_GET["oauth_verifier"]) ) {
 
 	// 連想配列をアルファベット順に並び替える
 	ksort( $params ) ;
+
 	// パラメータの連想配列を[キー=値&キー=値...]の文字列に変換する
 	$request_params = http_build_query( $params , "" , "&" ) ;
+ 
 	// 変換した文字列をURLエンコードする
 	$request_params = rawurlencode( $request_params ) ;
+ 
 	// リクエストメソッドをURLエンコードする
 	$encoded_request_method = rawurlencode( $request_method ) ;
+ 
 	// リクエストURLをURLエンコードする
 	$encoded_request_url = rawurlencode( $request_url ) ;
+ 
 	// リクエストメソッド、リクエストURL、パラメータを[&]で繋ぐ
 	$signature_data = $encoded_request_method . "&" . $encoded_request_url . "&" . $request_params ;
+
 	// キー[$signature_key]とデータ[$signature_data]を利用して、HMAC-SHA1方式のハッシュ値に変換する
 	$hash = hash_hmac( "sha1" , $signature_data , $signature_key , TRUE ) ;
+
 	// base64エンコードして、署名[$signature]が完成する
 	$signature = base64_encode( $hash ) ;
+
 	// パラメータの連想配列、[$params]に、作成した署名を加える
 	$params["oauth_signature"] = $signature ;
+
 	// パラメータの連想配列を[キー=値,キー=値,...]の文字列に変換する
 	$header_params = http_build_query( $params , "" , "," ) ;
 
@@ -284,7 +296,6 @@ if ( isset( $_GET['oauth_token'] ) || isset($_GET["oauth_verifier"]) ) {
 
 	// 取得したデータ
 	$response = substr( $res1, $res2["header_size"] ) ;	// 取得したデータ(JSONなど)
-	// TODO 不要だよね？
 	$header = substr( $res1, 0, $res2["header_size"] ) ;	// レスポンスヘッダー (検証に利用したい場合にどうぞ)
 
 	// [cURL]ではなく、[file_get_contents()]を使うには下記の通りです…
@@ -292,8 +303,8 @@ if ( isset( $_GET['oauth_token'] ) || isset($_GET["oauth_verifier"]) ) {
 
 	// リクエストトークンを取得できなかった場合
 	if( !$response ) {
-		echo "<p>何らかの理由で、リクエストトークンを取得できませんでした。申し訳ございません。</p>" ;
-		return ;
+		echo "<p>リクエストトークンを取得できませんでした…。$api_keyと$callback_url、そしてTwitterのアプリケーションに設定しているCallback URLを確認して下さい。</p>" ;
+		exit ;
 	}
 
 	// $responseの内容(文字列)を$query(配列)に直す
@@ -302,10 +313,11 @@ if ( isset( $_GET['oauth_token'] ) || isset($_GET["oauth_verifier"]) ) {
 	parse_str( $response, $query ) ;
 
 	// セッション[$_SESSION["oauth_token_secret"]]に[oauth_token_secret]を保存する
-	session_regenerate_id(true) ;
+	session_start() ;
+	session_regenerate_id( true ) ;
 	$_SESSION["oauth_token_secret"] = $query["oauth_token_secret"] ;
 
-	/*** ユーザーを認証画面へ飛ばす ***/
+	/*** [手順2] ユーザーを認証画面へ飛ばす ***/
 
 	// ユーザーを認証画面へ飛ばす (毎回ボタンを押す場合)
 	header( "Location: https://api.twitter.com/oauth/authorize?oauth_token=" . $query["oauth_token"] ) ;
@@ -315,5 +327,5 @@ if ( isset( $_GET['oauth_token'] ) || isset($_GET["oauth_verifier"]) ) {
 }
 
 ?>
-	</body>
+
 </html>
