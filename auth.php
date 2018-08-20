cce08604187ed1d4ab78725e206ac481cd2b23a1
hiroshi <dreamofank@yahoo.co.jp>
2018-08-20 22:27:51 +0900

    auth.phpのデバッグ中

--- auth.php
+++ auth.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <title>はーとぴっかー</title>
         <link rel="stylesheet/less" type="text/css" href="style.less">
-        <link rel="icon" href="favicon.ico">
+		<link rel="icon" href="favicon.ico">
         <meta name="viewport" content="width=device-width, user-scale=yes, initial-scale=1.0, maximum-scale=5.0" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
@@ -25,10 +25,10 @@
             <div class="container">
 
                 <h1>Heart♡Picker</h1>
-
+                
                 HeartPickerは自分や友達のいいねをランダムに表示するサービスです。<br>
-                TwitterIDと日時範囲(任意)を指定して、｢Heart Pick!｣ボタンを押してね！
-
+				TwitterIDと日時範囲(任意)を指定して、｢Heart Pick!｣ボタンを押してね！
+                
                 <form class="form" action="heart.php" method="post">
                     <label for="twitter_id" accesskey="n" class="item_EN">TwitterID　<span class="must">必須</span><br></label>
                     <div class="input-group">
@@ -77,6 +77,7 @@
                                 そこで、最低限の権限の認証で十分なため、read権限のみの認証を行います。
                                 以下は補足ですが、勝手にツイートがされてしまう、いわゆるスパムと呼ばれるものはread権限だけでなく、write権限を必要とします。
                                 そのため、HeartPickerはスパムと呼ばれるようなアプリの動作は権限の面で不可能となっています。</li>
+								</ol>
                             </div>
                         </details>
                         </ul>
@@ -98,221 +99,221 @@
             </ul>
         </div>
 <?php
-$api_key = '5L41MwG316NQvDhd3ru1UDiIa';
+$api_key = '5L41MwG316NQvDhd3ru1UDiIa'; 
 $api_secret = 'Y8daT5rjGsfQL49nHIzJKkL07Gq3BB2IAlR6NIl7owWSn00Lkz';
-$callback_url = "https://inputform.herokuapp.com/auth.php" ;    // Callback URL (このプログラムのURLアドレス)
+$callback_url = "https://inputform.herokuapp.com/auth.php" ;	// Callback URL (このプログラムのURLアドレス)
 
 // 認証画面から戻ってきた時 (認証OK)
 if ( isset( $_GET['oauth_token'] ) || isset($_GET["oauth_verifier"]) ) {
-    /*** アクセストークンを取得する ***/
-    //[リクエストトークン・シークレット]をセッションから取得
-    $request_token_secret = $_SESSION["oauth_token_secret"] ;
+	/*** アクセストークンを取得する ***/
+	//[リクエストトークン・シークレット]をセッションから取得
+	$request_token_secret = $_SESSION["oauth_token_secret"] ;
 
-    // アクセストークンを取得するAPI
-    $request_url = "https://api.twitter.com/oauth/access_token" ;
+	// アクセストークンを取得するAPI
+	$request_url = "https://api.twitter.com/oauth/access_token" ;
 
-    // リクエストメソッド
-    $request_method = "POST" ;
+	// リクエストメソッド
+	$request_method = "POST" ;
 
-    // キーを作成する
-    $signature_key = rawurlencode( $api_secret ) . "&" . rawurlencode( $request_token_secret ) ;
+	// キーを作成する
+	$signature_key = rawurlencode( $api_secret ) . "&" . rawurlencode( $request_token_secret ) ;
 
-    // パラメータ([oauth_signature]を除く)を連想配列で指定
-    $params = array(
-        "oauth_consumer_key" => $api_key ,
-        "oauth_token" => $_GET["oauth_token"] ,
-        "oauth_signature_method" => "HMAC-SHA1" ,
-        "oauth_timestamp" => time() ,
-        "oauth_verifier" => $_GET["oauth_verifier"] ,
-        "oauth_nonce" => microtime() ,
-        "oauth_version" => "1.0" ,
-    ) ;
+	// パラメータ([oauth_signature]を除く)を連想配列で指定
+	$params = array(
+		"oauth_consumer_key" => $api_key ,
+		"oauth_token" => $_GET["oauth_token"] ,
+		"oauth_signature_method" => "HMAC-SHA1" ,
+		"oauth_timestamp" => time() ,
+		"oauth_verifier" => $_GET["oauth_verifier"] ,
+		"oauth_nonce" => microtime() ,
+		"oauth_version" => "1.0" ,
+	) ;
 
-    // 配列の各パラメータの値をURLエンコード
-    foreach( $params as $key => $value ) {
-        $params[ $key ] = rawurlencode( $value ) ;
-    }
-    // 連想配列をアルファベット順に並び替え
-    ksort($params) ;
-    // パラメータの連想配列を[キー=値&キー=値...]の文字列に変換
-    $request_params = http_build_query( $params , "" , "&" ) ;
-    // 変換した文字列をURLエンコードする
-    $request_params = rawurlencode($request_params) ;
-    // リクエストメソッドをURLエンコードする
-    $encoded_request_method = rawurlencode( $request_method ) ;
-    // リクエストURLをURLエンコードする
-    $encoded_request_url = rawurlencode( $request_url ) ;
-    // リクエストメソッド、リクエストURL、パラメータを[&]で繋ぐ
-    $signature_data = $encoded_request_method . "&" . $encoded_request_url . "&" . $request_params ;
-    // キー[$signature_key]とデータ[$signature_data]を利用して、HMAC-SHA1方式のハッシュ値に変換する
-    $hash = hash_hmac( "sha1" , $signature_data , $signature_key , TRUE ) ;
-    // base64エンコードして、署名[$signature]が完成する
-    $signature = base64_encode( $hash ) ;
-    // パラメータの連想配列、[$params]に、作成した署名を加える
-    $params["oauth_signature"] = $signature ;
-    // パラメータの連想配列を[キー=値,キー=値,...]の文字列に変換する
-    $header_params = http_build_query( $params, "", "," ) ;
+	// 配列の各パラメータの値をURLエンコード
+	foreach( $params as $key => $value ) {
+		$params[ $key ] = rawurlencode( $value ) ;
+	}
+	// 連想配列をアルファベット順に並び替え
+	ksort($params) ;
+	// パラメータの連想配列を[キー=値&キー=値...]の文字列に変換
+	$request_params = http_build_query( $params , "" , "&" ) ;
+	// 変換した文字列をURLエンコードする
+	$request_params = rawurlencode($request_params) ;
+	// リクエストメソッドをURLエンコードする
+	$encoded_request_method = rawurlencode( $request_method ) ;
+	// リクエストURLをURLエンコードする
+	$encoded_request_url = rawurlencode( $request_url ) ;
+	// リクエストメソッド、リクエストURL、パラメータを[&]で繋ぐ
+	$signature_data = $encoded_request_method . "&" . $encoded_request_url . "&" . $request_params ;
+	// キー[$signature_key]とデータ[$signature_data]を利用して、HMAC-SHA1方式のハッシュ値に変換する
+	$hash = hash_hmac( "sha1" , $signature_data , $signature_key , TRUE ) ;
+	// base64エンコードして、署名[$signature]が完成する
+	$signature = base64_encode( $hash ) ;
+	// パラメータの連想配列、[$params]に、作成した署名を加える
+	$params["oauth_signature"] = $signature ;
+	// パラメータの連想配列を[キー=値,キー=値,...]の文字列に変換する
+	$header_params = http_build_query( $params, "", "," ) ;
 
-    // リクエスト用のコンテキストを作成する
-    $context = array(
-        "http" => array(
-            "method" => $request_method ,   //リクエストメソッド
-            "header" => array(  //カスタムヘッダー
-                "Authorization: OAuth " . $header_params ,
-            ) ,
-        ) ,
-    ) ;
+	// リクエスト用のコンテキストを作成する
+	$context = array(
+		"http" => array(
+			"method" => $request_method ,	//リクエストメソッド
+			"header" => array(	//カスタムヘッダー
+				"Authorization: OAuth " . $header_params ,
+			) ,
+		) ,
+	) ;
 
-    // cURLを使ってリクエスト
-    $curl = curl_init() ;
-    curl_setopt( $curl, CURLOPT_URL , $request_url ) ;
-    curl_setopt( $curl, CURLOPT_HEADER, 1 ) ;
-    curl_setopt( $curl, CURLOPT_CUSTOMREQUEST , $context["http"]["method"] ) ;  // メソッド
-    curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER , false ) ;  // 証明書の検証を行わない
-    curl_setopt( $curl, CURLOPT_RETURNTRANSFER , true ) ;   // curl_execの結果を文字列で返す
-    curl_setopt( $curl, CURLOPT_HTTPHEADER , $context["http"]["header"] ) ; // ヘッダー
-    curl_setopt( $curl, CURLOPT_TIMEOUT , 5 ) ; // タイムアウトの秒数
-    $res1 = curl_exec( $curl ) ;
-    $res2 = curl_getinfo( $curl ) ;
-    curl_close( $curl ) ;
+	// cURLを使ってリクエスト
+	$curl = curl_init() ;
+	curl_setopt( $curl, CURLOPT_URL , $request_url ) ;
+	curl_setopt( $curl, CURLOPT_HEADER, 1 ) ; 
+	curl_setopt( $curl, CURLOPT_CUSTOMREQUEST , $context["http"]["method"] ) ;	// メソッド
+	curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER , false ) ;	// 証明書の検証を行わない
+	curl_setopt( $curl, CURLOPT_RETURNTRANSFER , true ) ;	// curl_execの結果を文字列で返す
+	curl_setopt( $curl, CURLOPT_HTTPHEADER , $context["http"]["header"] ) ;	// ヘッダー
+	curl_setopt( $curl, CURLOPT_TIMEOUT , 5 ) ;	// タイムアウトの秒数
+	$res1 = curl_exec( $curl ) ;
+	$res2 = curl_getinfo( $curl ) ;
+	curl_close( $curl ) ;
 
-    // 取得したデータ
-    $response = substr( $res1, $res2["header_size"] ) ;
+	// 取得したデータ
+	$response = substr( $res1, $res2["header_size"] ) ;
 
-    // $responseの内容(文字列)を$query(配列)に直す
-    // aaa=AAA&bbb=BBB → [ "aaa"=>"AAA", "bbb"=>"BBB" ]
-    $query = [] ;
-    parse_str( $response, $query ) ;
+	// $responseの内容(文字列)を$query(配列)に直す
+	// aaa=AAA&bbb=BBB → [ "aaa"=>"AAA", "bbb"=>"BBB" ]
+	$query = [] ;
+	parse_str( $response, $query ) ;
 
-    // 取得したアクセストークンをセッションの変数に格納
-    $_SESSION["oauth_token"] = $query["oauth_token"];
+	// 取得したアクセストークンをセッションの変数に格納
+	$_SESSION["oauth_token"] = $query["oauth_token"];
 
-    // 取得したアクセストークン・シークレットをセッションの変数に格納
+	// 取得したアクセストークン・シークレットをセッションの変数に格納
     $_SESSION["oauth_token_secret"] = $query["oauth_token_secret"];
+    
+	// ユーザーID
+	// $query["user_id"]
 
-    // ユーザーID
-    // $query["user_id"]
-
-    // スクリーンネーム
-    // $query["screen_name"]
+	// スクリーンネーム
+	// $query["screen_name"]
 
 // 認証画面から戻ってきた時 (認証NG) 特に何も処理をしない
 } elseif ( isset( $_GET["denied"] ) ) {
-    // TODO return でOK?
-    exit ;
+	// TODO return でOK?
+	exit ;
 // 初回のアクセス
 } else {
-    /*** リクエストトークンの取得 ***/
+	/*** リクエストトークンの取得 ***/
 
-    // [アクセストークンシークレット] (まだ存在しないので空文字)
-    $access_token_secret = "" ;
+	// [アクセストークンシークレット] (まだ存在しないので空文字)
+	$access_token_secret = "" ;
 
-    // リクエストトークンを取得するAPI
-    $request_url = "https://api.twitter.com/oauth/request_token" ;
+	// リクエストトークンを取得するAPI
+	$request_url = "https://api.twitter.com/oauth/request_token" ;
 
-    // リクエストメソッド
-    $request_method = "POST" ;
+	// リクエストメソッド
+	$request_method = "POST" ;
 
-    // キーを作成する (URLエンコードする)
-    $signature_key = rawurlencode( $api_secret ) . "&" . rawurlencode( $access_token_secret ) ;
+	// キーを作成する (URLエンコードする)
+	$signature_key = rawurlencode( $api_secret ) . "&" . rawurlencode( $access_token_secret ) ;
 
-    // パラメータ([oauth_signature]を除く)を連想配列で指定
-    $params = array(
-        "oauth_callback" => $callback_url ,
-        "oauth_consumer_key" => $api_key ,
-        "oauth_signature_method" => "HMAC-SHA1" ,
-        "oauth_timestamp" => time() ,
-        "oauth_nonce" => microtime() ,
-        "oauth_version" => "1.0" ,
-    ) ;
+	// パラメータ([oauth_signature]を除く)を連想配列で指定
+	$params = array(
+		"oauth_callback" => $callback_url ,
+		"oauth_consumer_key" => $api_key ,
+		"oauth_signature_method" => "HMAC-SHA1" ,
+		"oauth_timestamp" => time() ,
+		"oauth_nonce" => microtime() ,
+		"oauth_version" => "1.0" ,
+	) ;
 
-    // 各パラメータをURLエンコードする
-    foreach( $params as $key => $value ) {
-        // コールバックURLはエンコードしない
-        if( $key == "oauth_callback" ) {
-            continue ;
-        }
+	// 各パラメータをURLエンコードする
+	foreach( $params as $key => $value ) {
+		// コールバックURLはエンコードしない
+		if( $key == "oauth_callback" ) {
+			continue ;
+		}
 
-        // URLエンコード処理
-        $params[ $key ] = rawurlencode( $value ) ;
-    }
+		// URLエンコード処理
+		$params[ $key ] = rawurlencode( $value ) ;
+	}
 
-    // 連想配列をアルファベット順に並び替える
-    ksort( $params ) ;
-    // パラメータの連想配列を[キー=値&キー=値...]の文字列に変換する
-    $request_params = http_build_query( $params , "" , "&" ) ;
-    // 変換した文字列をURLエンコードする
-    $request_params = rawurlencode( $request_params ) ;
-    // リクエストメソッドをURLエンコードする
-    $encoded_request_method = rawurlencode( $request_method ) ;
-    // リクエストURLをURLエンコードする
-    $encoded_request_url = rawurlencode( $request_url ) ;
-    // リクエストメソッド、リクエストURL、パラメータを[&]で繋ぐ
-    $signature_data = $encoded_request_method . "&" . $encoded_request_url . "&" . $request_params ;
-    // キー[$signature_key]とデータ[$signature_data]を利用して、HMAC-SHA1方式のハッシュ値に変換する
-    $hash = hash_hmac( "sha1" , $signature_data , $signature_key , TRUE ) ;
-    // base64エンコードして、署名[$signature]が完成する
-    $signature = base64_encode( $hash ) ;
-    // パラメータの連想配列、[$params]に、作成した署名を加える
-    $params["oauth_signature"] = $signature ;
-    // パラメータの連想配列を[キー=値,キー=値,...]の文字列に変換する
-    $header_params = http_build_query( $params , "" , "," ) ;
+	// 連想配列をアルファベット順に並び替える
+	ksort( $params ) ;
+	// パラメータの連想配列を[キー=値&キー=値...]の文字列に変換する
+	$request_params = http_build_query( $params , "" , "&" ) ;
+	// 変換した文字列をURLエンコードする
+	$request_params = rawurlencode( $request_params ) ;
+	// リクエストメソッドをURLエンコードする
+	$encoded_request_method = rawurlencode( $request_method ) ;
+	// リクエストURLをURLエンコードする
+	$encoded_request_url = rawurlencode( $request_url ) ;
+	// リクエストメソッド、リクエストURL、パラメータを[&]で繋ぐ
+	$signature_data = $encoded_request_method . "&" . $encoded_request_url . "&" . $request_params ;
+	// キー[$signature_key]とデータ[$signature_data]を利用して、HMAC-SHA1方式のハッシュ値に変換する
+	$hash = hash_hmac( "sha1" , $signature_data , $signature_key , TRUE ) ;
+	// base64エンコードして、署名[$signature]が完成する
+	$signature = base64_encode( $hash ) ;
+	// パラメータの連想配列、[$params]に、作成した署名を加える
+	$params["oauth_signature"] = $signature ;
+	// パラメータの連想配列を[キー=値,キー=値,...]の文字列に変換する
+	$header_params = http_build_query( $params , "" , "," ) ;
 
-    // リクエスト用のコンテキストを作成する
-    $context = array(
-        "http" => array(
-            "method" => $request_method , // リクエストメソッド (POST)
-            "header" => array(            // カスタムヘッダー
-                "Authorization: OAuth " . $header_params ,
-            ) ,
-        ) ,
-    ) ;
+	// リクエスト用のコンテキストを作成する
+	$context = array(
+		"http" => array(
+			"method" => $request_method , // リクエストメソッド (POST)
+			"header" => array(			  // カスタムヘッダー
+				"Authorization: OAuth " . $header_params ,
+			) ,
+		) ,
+	) ;
 
-    // cURLを使ってリクエスト
-    $curl = curl_init() ;
-    curl_setopt( $curl, CURLOPT_URL , $request_url ) ;  // リクエストURL
-    curl_setopt( $curl, CURLOPT_HEADER, true ) ;    // ヘッダーを取得する
-    curl_setopt( $curl, CURLOPT_CUSTOMREQUEST , $context["http"]["method"] ) ;  // メソッド
-    curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER , false ) ;  // 証明書の検証を行わない
-    curl_setopt( $curl, CURLOPT_RETURNTRANSFER , true ) ;   // curl_execの結果を文字列で返す
-    curl_setopt( $curl, CURLOPT_HTTPHEADER , $context["http"]["header"] ) ; // リクエストヘッダーの内容
-    curl_setopt( $curl, CURLOPT_TIMEOUT , 5 ) ; // タイムアウトの秒数
-    $res1 = curl_exec( $curl ) ;
-    $res2 = curl_getinfo( $curl ) ;
-    curl_close( $curl ) ;
+	// cURLを使ってリクエスト
+	$curl = curl_init() ;
+	curl_setopt( $curl, CURLOPT_URL , $request_url ) ;	// リクエストURL
+	curl_setopt( $curl, CURLOPT_HEADER, true ) ;	// ヘッダーを取得する
+	curl_setopt( $curl, CURLOPT_CUSTOMREQUEST , $context["http"]["method"] ) ;	// メソッド
+	curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER , false ) ;	// 証明書の検証を行わない
+	curl_setopt( $curl, CURLOPT_RETURNTRANSFER , true ) ;	// curl_execの結果を文字列で返す
+	curl_setopt( $curl, CURLOPT_HTTPHEADER , $context["http"]["header"] ) ;	// リクエストヘッダーの内容
+	curl_setopt( $curl, CURLOPT_TIMEOUT , 5 ) ;	// タイムアウトの秒数
+	$res1 = curl_exec( $curl ) ;
+	$res2 = curl_getinfo( $curl ) ;
+	curl_close( $curl ) ;
 
-    // 取得したデータ
-    $response = substr( $res1, $res2["header_size"] ) ; // 取得したデータ(JSONなど)
-    // TODO 不要だよね？
-    $header = substr( $res1, 0, $res2["header_size"] ) ;    // レスポンスヘッダー (検証に利用したい場合にどうぞ)
+	// 取得したデータ
+	$response = substr( $res1, $res2["header_size"] ) ;	// 取得したデータ(JSONなど)
+	// TODO 不要だよね？
+	$header = substr( $res1, 0, $res2["header_size"] ) ;	// レスポンスヘッダー (検証に利用したい場合にどうぞ)
 
-    // [cURL]ではなく、[file_get_contents()]を使うには下記の通りです…
-    // $response = file_get_contents( $request_url , false , stream_context_create( $context ) ) ;
+	// [cURL]ではなく、[file_get_contents()]を使うには下記の通りです…
+	// $response = file_get_contents( $request_url , false , stream_context_create( $context ) ) ;
 
-    // リクエストトークンを取得できなかった場合
-    if( !$response ) {
-        echo "<p>何らかの理由で、リクエストトークンを取得できませんでした。申し訳ございません。</p>" ;
-        return ;
-    }
+	// リクエストトークンを取得できなかった場合
+	if( !$response ) {
+		echo "<p>何らかの理由で、リクエストトークンを取得できませんでした。申し訳ございません。</p>" ;
+		return ;
+	}
 
-    // $responseの内容(文字列)を$query(配列)に直す
-    // aaa=AAA&bbb=BBB → [ "aaa"=>"AAA", "bbb"=>"BBB" ]
-    $query = [] ;
-    parse_str( $response, $query ) ;
+	// $responseの内容(文字列)を$query(配列)に直す
+	// aaa=AAA&bbb=BBB → [ "aaa"=>"AAA", "bbb"=>"BBB" ]
+	$query = [] ;
+	parse_str( $response, $query ) ;
 
-    // セッション[$_SESSION["oauth_token_secret"]]に[oauth_token_secret]を保存する
-    session_regenerate_id(true) ;
-    $_SESSION["oauth_token_secret"] = $query["oauth_token_secret"] ;
+	// セッション[$_SESSION["oauth_token_secret"]]に[oauth_token_secret]を保存する
+	session_regenerate_id(true) ;
+	$_SESSION["oauth_token_secret"] = $query["oauth_token_secret"] ;
 
-    /*** ユーザーを認証画面へ飛ばす ***/
+	/*** ユーザーを認証画面へ飛ばす ***/
 
-    // ユーザーを認証画面へ飛ばす (毎回ボタンを押す場合)
-    header( "Location: https://api.twitter.com/oauth/authorize?oauth_token=" . $query["oauth_token"] ) ;
+	// ユーザーを認証画面へ飛ばす (毎回ボタンを押す場合)
+	header( "Location: https://api.twitter.com/oauth/authorize?oauth_token=" . $query["oauth_token"] ) ;
 
-    // ユーザーを認証画面へ飛ばす (二回目以降は認証画面をスキップする場合)
-    // header( "Location: https://api.twitter.com/oauth/authenticate?oauth_token=" . $query["oauth_token"] ) ;
+	// ユーザーを認証画面へ飛ばす (二回目以降は認証画面をスキップする場合)
+	// header( "Location: https://api.twitter.com/oauth/authenticate?oauth_token=" . $query["oauth_token"] ) ;
 }
 
 ?>
-    </body>
+	</body>
 </html>
