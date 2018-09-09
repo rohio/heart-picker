37c081434a341985e51334d2e65461afcfa492d3
hiroshi <dreamofank@yahoo.co.jp>
2018-09-09 21:52:03 +0900

    デザインの修正。日付のデバック中

--- logic.php
+++ logic.php
@@ -497,7 +497,7 @@ while(true){
 			echo("TwitterAPIの使用回数の上限に達したため、いいねを取得できません。<br>");
 			echo("oauth_token:" . isset($_SESSION["oauth_token"]));
 			echo("oauth_token_secret:" . isset($_SESSION["oauth_token_secret"]));
-			if(isset($_SESSION["oauth_token"]) && isset($_SESSION["oauth_token_secret"]) == NULL){
+			if(isset($_SESSION["oauth_token"]) == NULL && isset($_SESSION["oauth_token_secret"]) == NULL){
 				echo("上限を緩和したい場合は、ページ下部の｢詳細の使い方｣内にあるTwiiterのアイコンをクリックして、Twitterでログインを行ってください。");
 			}
 		} else {
