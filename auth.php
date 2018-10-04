806688d58b8812c305b314420e2e797d51e80455
hiroshi <dreamofank@yahoo.co.jp>
2018-10-05 01:56:02 +0900

    全角を入力された場合、半角に自動的に変換するように修正

--- auth.php
+++ auth.php
@@ -220,15 +220,50 @@ if ( isset( $_GET['oauth_token'] ) || isset($_GET["oauth_verifier"]) ) {
         <script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script type="text/javascript">
-            function checkForm($this)
+            // TwitterIDのテキストボックスに、全角半角数字、全角半角英字、全角半角アンダースコアの入力のみ受付
+            function checkID($this)
             {
                 var str=$this.value;
-                while(str.match(/[^\d\-\/]/))
+                while(str.match(/[^\d０-９A-ZＡ-Ｚa-zａ-ｚ_＿]/))
                 {
-                    str=str.replace(/[^\d\-\/]/,"");
+                    str=str.replace(/[^\d０-９A-ZＡ-Ｚa-zａ-ｚ_＿]/,"");
                 }
                 $this.value=str;
             }
+
+            // TwitterIDのテキストボックスに、全角半角数字、全角半角英字、全角半角アンダースコアが入力された場合、半角に変換
+            $(function(){
+                $("#twitter_id").change(function(){
+                    var str = $(this).val();
+                    str = str.replace( /[Ａ-Ｚａ-ｚ０-９＿]/g, function(s) {
+                        return String.fromCharCode(s.charCodeAt(0) - 65248);
+                    })
+                    $(this).val(str);
+                }).change();
+            });
+
+            // 日付範囲のテキストボックスに、全角半角数字、全角半角ハイフン、全角半角スラッシュの入力のみ受付
+            function checkForm($this)
+            {
+                var str=$this.value;
+                while(str.match(/[^\d\-\/０-９ー－―／]/))
+                {
+                    str=str.replace(/[^\d\-\/０-９ー－―／]/,"");
+                }
+                $this.value=str;
+            }
+
+            // 日付範囲のテキストボックスに、全角数字、全角ハイフン、全角スラッシュが入力された場合、半角に変換
+            $(function(){
+                $("input.user_input").change(function(){
+                    var str = $(this).val();
+                    str = str.replace( /[０-９－／]/g, function(s) {
+                        return String.fromCharCode(s.charCodeAt(0) - 65248);
+                    })
+                    .replace(/[‐－―ー]/g, "-");
+                    $(this).val(str);
+                }).change();
+            });
         </script>
     </head>
     <body>
--- index.php
+++ index.php
@@ -9,15 +9,50 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script type="text/javascript">
-            function checkForm($this)
+            // TwitterIDのテキストボックスに、全角半角数字、全角半角英字、全角半角アンダースコアの入力のみ受付
+            function checkID($this)
             {
                 var str=$this.value;
-                while(str.match(/[^\d\-\/]/))
+                while(str.match(/[^\d０-９A-ZＡ-Ｚa-zａ-ｚ_＿]/))
                 {
-                    str=str.replace(/[^\d\-\/]/,"");
+                    str=str.replace(/[^\d０-９A-ZＡ-Ｚa-zａ-ｚ_＿]/,"");
                 }
                 $this.value=str;
             }
+
+            // TwitterIDのテキストボックスに、全角半角数字、全角半角英字、全角半角アンダースコアが入力された場合、半角に変換
+            $(function(){
+                $("#twitter_id").change(function(){
+                    var str = $(this).val();
+                    str = str.replace( /[Ａ-Ｚａ-ｚ０-９＿]/g, function(s) {
+                        return String.fromCharCode(s.charCodeAt(0) - 65248);
+                    })
+                    $(this).val(str);
+                }).change();
+            });
+
+            // 日付範囲のテキストボックスに、全角半角数字、全角半角ハイフン、全角半角スラッシュの入力のみ受付
+            function checkForm($this)
+            {
+                var str=$this.value;
+                while(str.match(/[^\d\-\/０-９ー－―／]/))
+                {
+                    str=str.replace(/[^\d\-\/０-９ー－―／]/,"");
+                }
+                $this.value=str;
+            }
+
+            // 日付範囲のテキストボックスに、全角数字、全角ハイフン、全角スラッシュが入力された場合、半角に変換
+            $(function(){
+                $("input.user_input").change(function(){
+                    var str = $(this).val();
+                    str = str.replace( /[０-９－／]/g, function(s) {
+                        return String.fromCharCode(s.charCodeAt(0) - 65248);
+                    })
+                    .replace(/[‐－―ー]/g, "-");
+                    $(this).val(str);
+                }).change();
+            });
         </script>
     </head>
     <body>
@@ -33,7 +68,7 @@
                     <label for="twitter_id" accesskey="n" class="item_EN">TwitterID　<span class="must">必須</span><br></label>
                     <div class="input-group">
                         <span class="input-group__addon">@</span>
-                        <input type="text" name="twitter_id" id="twitter_id" class="input-group__control" placeholder="例: TwitterJP" required>
+                        <input type="text" name="twitter_id" id="twitter_id" class="input-group__control" placeholder="例: TwitterJP" onInput="checkID(this)" required>
                     </div>
                     <br>
 
