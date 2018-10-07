287f3a09515a5698bc9514c8ce5852f166599e87
hiroshi <dreamofank@yahoo.co.jp>
2018-10-07 16:12:31 +0900

    iosのスクロール固定のコードが不要なので削除

--- heart.php
+++ heart.php
@@ -35,11 +35,6 @@
             $('head').append(
                 '<style type="text/css">#wrapper { display: none; } #fade, #loader { display: block; }</style>'
             );
-
-            // スクロールを無効にする(iOS用)
-            $(window).on('touchmove.noScroll', function(e) {
-                e.preventDefault();
-            });
             
             jQuery.event.add(window,"load",function() { // 全ての読み込み完了後に呼ばれる関数
                 var pageH = $("#wrapper").height();
@@ -47,9 +42,6 @@
                 $("#fade").css("height", pageH).delay(900).fadeOut(800);
                 $("#loader").delay(600).fadeOut(300);
                 $("#wrapper").css("display", "block");
-
-                // スクロール無効を解除する(iOS用)
-                $(window).off('.noScroll');
             });
         </script>
     </head>
