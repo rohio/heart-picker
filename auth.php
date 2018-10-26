e27b6300dff1110f7925d9004630190c6ce3626d
hiroshi <dreamofank@yahoo.co.jp>
2018-10-26 20:55:50 +0900

    ログインボタンの追加。認証のときに毎回ボタンを押すように修正

--- auth.php
+++ auth.php
@@ -202,10 +202,10 @@ if ( isset( $_GET['oauth_token'] ) || isset($_GET["oauth_verifier"]) ) {
 	/*** ユーザーを認証画面へ飛ばす ***/
 
 	// ユーザーを認証画面へ飛ばす (毎回ボタンを押す場合)
-	// header( "Location: https://api.twitter.com/oauth/authorize?oauth_token=" . $query["oauth_token"] ) ;
+	header( "Location: https://api.twitter.com/oauth/authorize?oauth_token=" . $query["oauth_token"] ) ;
 
 	// ユーザーを認証画面へ飛ばす (二回目以降は認証画面をスキップする場合)
-	header( "Location: https://api.twitter.com/oauth/authenticate?oauth_token=" . $query["oauth_token"] ) ;
+	// header( "Location: https://api.twitter.com/oauth/authenticate?oauth_token=" . $query["oauth_token"] ) ;
 }
 ?>
 
